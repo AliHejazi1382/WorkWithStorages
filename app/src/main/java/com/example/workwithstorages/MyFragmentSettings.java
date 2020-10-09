a2b320e705d0b5afb0b1fdea4036b5deb9c26fa6
AliHejazi1382 <mrhjz08@gmail.com>
2020-10-09 16:21:42 +0330

    the settings screen created

---
+++ app/src/main/java/com/example/workwithstorages/MyFragmentSettings.java
@@ -0,0 +1,71 @@
+package com.example.workwithstorages;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreference;
+import androidx.preference.SwitchPreferenceCompat;
+
+public class MyFragmentSettings extends PreferenceFragmentCompat {
+    private SharedPreferences preferences;
+    private Context context;
+    private PreferenceScreen screen;
+    private SwitchPreferenceCompat encryptedNotes;
+    private PreferenceCategory security;
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        initFields();
+        loadFields();
+        makeFields();
+        setPreferenceScreen(screen);
+    }
+    
+    private void initFields() {
+        this.context = getPreferenceManager().getContext();
+        this.screen = getPreferenceManager().createPreferenceScreen(this.context);
+        this.preferences = PreferenceManager.getDefaultSharedPreferences(this.context);
+        this.encryptedNotes = new SwitchPreferenceCompat(this.context);
+        this.encryptedNotes.setKey("encryptedNotes");
+        this.security = new PreferenceCategory(this.context);
+        this.security.setKey("security");
+    }
+
+    private void loadFields() {
+        if (preferences.contains("encryptedNotes")){
+            boolean isChecked = preferences.getBoolean("encryptedNotes", false);
+            if (isChecked){
+                encryptedNotes.setIcon(R.drawable.encrypted);
+            } else {
+                encryptedNotes.setIcon(R.drawable.no_encryption);
+            }
+        }
+    }
+
+    private void makeFields() {
+        encryptedNotes.setSummaryOff("The notes you save will not be encrypted.(Recommended)");
+        encryptedNotes.setSummaryOn("The notes you save will not be encrypted\nThis may take up too much of your memory");
+        encryptedNotes.setTitle("Encrypted Notes");
+        encryptedNotes.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                boolean isChecked = (boolean) newValue;
+                if (isChecked){
+                    encryptedNotes.setIcon(R.drawable.encrypted);
+                } else {
+                    encryptedNotes.setIcon(R.drawable.no_encryption);
+                }
+                return true;
+            }
+        });
+        security.setTitle("Encrypted Data");
+        screen.addPreference(security);
+        security.addPreference(encryptedNotes);
+    }
+}
--- app/src/main/java/com/example/workwithstorages/SettingsActivity.java
+++ app/src/main/java/com/example/workwithstorages/SettingsActivity.java
@@ -10,5 +10,6 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        getSupportFragmentManager().beginTransaction().replace(R.id.layout, new MyFragmentSettings()).commit();
     }
 }
